c2d94bab8ab9c14e7d38eefb080bcd04e7968b9f
tuantm <tuantm@omegatheme.com>
2024-11-11 10:09:44 +0700

    optimize db connection

--- kafka-listener/main.go
+++ kafka-listener/main.go
@@ -65,21 +65,10 @@ func updateEvent(event model.Event) (*model.Event, error) {
 	return &response, nil
 }
 
-func saveEventInDb(updatedEvent *model.Event, tracking model.TrackingEvent) error {
+func saveEventInDb(client *mongo.Client, updatedEvent *model.Event, tracking model.TrackingEvent) error {
 	if updatedEvent == nil {
 		return fmt.Errorf("event is nil")
 	}
-	clientOptions := options.Client().ApplyURI(fmt.Sprintf("mongodb://%s", os.Getenv("MONGO_URI")))
-	client, err := mongo.Connect(context.Background(), clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func(client *mongo.Client, ctx context.Context) {
-		err := client.Disconnect(ctx)
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-	}(client, context.Background())
 
 	db := client.Database(os.Getenv("MONGO_DB"))
 	collection := db.Collection(os.Getenv("MONGO_COLLECTION"))
@@ -118,6 +107,63 @@ func saveEventInDb(updatedEvent *model.Event, tracking model.TrackingEvent) erro
 	return nil
 }
 
+func createTopicIfNotExists(admin *kafka.AdminClient, topic string) {
+	metadata, err := admin.GetMetadata(&topic, false, 5000)
+	if err != nil {
+		log.Fatalf("Failed to get metadata: %s", err)
+	}
+
+	if _, exists := metadata.Topics[topic]; exists {
+		fmt.Printf("Topic %s already exists\n", topic)
+		return
+	}
+
+	topics := []kafka.TopicSpecification{{
+		Topic:             topic,
+		NumPartitions:     1,
+		ReplicationFactor: 1,
+	}}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	results, err := admin.CreateTopics(ctx, topics)
+	if err != nil {
+		log.Fatalf("Failed to create topic: %s", err)
+	}
+
+	for _, result := range results {
+		if result.Error.Code() != kafka.ErrNoError {
+			log.Fatalf("Failed to create topic %s: %v\n", result.Topic, result.Error)
+		}
+		fmt.Printf("Topic %s created successfully\n", result.Topic)
+	}
+}
+
+func checkAndCreateCollection(client *mongo.Client, dbName, collectionName string) (bool, error) {
+	// List collections in the database
+	db := client.Database(dbName)
+	collections, err := db.ListCollectionNames(context.TODO(), map[string]interface{}{})
+	if err != nil {
+		return false, fmt.Errorf("failed to list collections: %w", err)
+	}
+
+	// Check if the collection already exists
+	for _, col := range collections {
+		if col == collectionName {
+			return true, nil
+		}
+	}
+
+	// Create the collection if it doesn't exist
+	err = db.CreateCollection(context.TODO(), collectionName)
+	if err != nil {
+		return false, fmt.Errorf("failed to create collection: %w", err)
+	}
+
+	return false, nil
+}
+
 func main() {
 	kafkaBroker = os.Getenv("KAFKA_BROKER")
 	topic = os.Getenv("KAFKA_TOPIC")
@@ -125,6 +171,41 @@ func main() {
 	fmt.Println("Kafka Broker: ", kafkaBroker)
 	fmt.Println("Kafka Topic: ", topic)
 	fmt.Println("Kafka Group ID: ", groupID)
+
+	clientOptions := options.Client().ApplyURI(fmt.Sprintf("mongodb://%s", os.Getenv("MONGO_URI")))
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(client *mongo.Client, ctx context.Context) {
+		err := client.Disconnect(ctx)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+	}(client, context.Background())
+
+	dbName := os.Getenv("MONGO_DB")
+	collectionName := os.Getenv("MONGO_COLLECTION")
+
+	collectionExists, err := checkAndCreateCollection(client, dbName, collectionName)
+	if err != nil {
+		log.Fatalf("Failed to check or create collection: %s", err)
+	}
+
+	if collectionExists {
+		fmt.Printf("Collection %s already exists in database %s\n", collectionName, dbName)
+	} else {
+		fmt.Printf("Collection %s created in database %s\n", collectionName, dbName)
+	}
+
+	adminClient, err := kafka.NewAdminClient(&kafka.ConfigMap{"bootstrap.servers": kafkaBroker})
+	if err != nil {
+		log.Fatalf("Failed to create Admin client: %s", err)
+	}
+	defer adminClient.Close()
+
+	// Check if topic exists and create if not
+	createTopicIfNotExists(adminClient, topic)
 
 	kafkaBrokerServer, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": kafkaBroker})
 	if err != nil {
@@ -189,7 +270,7 @@ func main() {
 					fmt.Println(err)
 				}
 
-				err = saveEventInDb(updatedEvent, tracking)
+				err = saveEventInDb(client, updatedEvent, tracking)
 				if err != nil {
 					fmt.Println(err)
 				}
